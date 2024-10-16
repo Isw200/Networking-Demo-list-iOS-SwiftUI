b91e2e94e5c36cd7111f6ecb6c4a5ad604907922
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-10-16 18:44:54 +0530

    Initial Commit

---
+++ NetworkingDemoTests/NetworkingDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingDemoTests.swift
+//  NetworkingDemoTests
+//
+//  Created by Isuru Ariyarathna on 2024-10-16.
+//
+
+import Testing
+@testable import NetworkingDemo
+
+struct NetworkingDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
