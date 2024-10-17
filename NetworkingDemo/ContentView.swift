c9e899ae452930d8c41ad2cd7d4664882ba2ea3c
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-10-17 08:56:53 +0530

    done

--- NetworkingDemo/ContentView.swift
+++ NetworkingDemo/ContentView.swift
@@ -7,15 +7,81 @@
 
 import SwiftUI
 
+struct DataDTO : Codable, Hashable {
+    let postId: Int
+    let title: String
+    let body: String
+    
+    enum CodingKeys: String, CodingKey {
+        case postId = "id"
+        case title
+        case body
+    }
+}
+
 struct ContentView: View {
+    @State var data : [DataDTO] = []
+    @State var isLoading: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack{
+                if isLoading {
+                    VStack {
+                        ProgressView()
+                    }
+                } else {
+                    List(data, id: \.postId) { item in
+                        VStack(alignment: .leading) {
+                            Text(item.title)
+                                .font(.headline)
+                            Text(item.body)
+                                .font(.subheadline)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("News Feed")
         }
-        .padding()
+        .refreshable {
+            Task {
+                await fetchData()
+            }
+        }
+        .onAppear{
+            Task {
+                await fetchData()
+            }
+        }
+    }
+    
+    func fetchData() async {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        
+        guard let unwrappedUrl = url else { return }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: unwrappedUrl)
+            guard let response = response as? HTTPURLResponse else {
+                print("Something went wrong")
+                return
+            }
+            
+            switch response.statusCode {
+            case 200..<300:
+                let decodedData = try JSONDecoder().decode([DataDTO].self, from: data)
+                self.data = decodedData
+                
+            case 400..<500:
+                print("Server Error")
+            default:
+                print("Something went wrong")
+            }
+            
+        } catch {
+            print("Something went wrong \(error.localizedDescription)")
+        }
+        self.isLoading = false
     }
 }
 
